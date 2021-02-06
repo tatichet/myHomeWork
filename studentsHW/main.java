9c4331ff3cad2e68e634b3af307c0947b480a843
tatichet <chetvernia.t@gmail.com>
2021-02-06 19:45:28 +0200

    students final changes

--- studentsHW/main.java
+++ studentsHW/main.java
@@ -14,12 +14,37 @@ public class main {
 		st2.lastName = "Foros";
 		st2.rating = 80;
 		
+		Student st3 = new Student();
+		st3.firstName = "Anna";
+		st3.lastName = "Goron";
+		st3.rating = 80;
 		
-		//st1.print();
-		//System.out.println("******");
-		//st2.print();
-		//System.out.println("******");
+		  boolean testResult = st1.isbetterStudent(st1, st2);
+  
+		System.out.println("Is Olga Glushko better student?");
+		System.out.println(testResult);
+		System.out.println("-------");
+		System.out.println("List of studets");
 	
-	}	 
+		st1.print();
+		System.out.println("******");
+		st2.print();
+		System.out.println("******");
+		st3.print();
+		
+		
+		int aver = ((st1.rating+st2.rating+st3.rating)/3);
+		System.out.println("Average rating of all students is " + aver);
+		System.out.println("******");
+		
+		st3.print("New rating for Anna Goron", 90);
+		
+		st3.rating = 90;
+		int aver2 = ((st1.rating+st2.rating+st3.rating)/3);
+		System.out.println("New average rating of all students is " + aver2);
+	}
 }
+
+	
+
      
--- studentsHW/student.java
+++ studentsHW/student.java
@@ -5,19 +5,26 @@ public class Student {
    String lastName;
    int rating;
    
-   
    public boolean isbetterStudent(Student st1, Student st2) {
-		  if(st1.rating > st2.rating) {			 
-	      return true;	   
-		   }else {			 
-			   return false;
-		   }
-	   }
+	  if(st1.rating > st2.rating) {			 
+	     return true;	   
+		  }else {			 
+		   return false;
+		  }
+	  }
+   public void print() {
+	   System.out.println("First name:" + firstName);
+	   System.out.println("Last name:" + lastName);
+	   System.out.println("Rating:" + rating);
+	  
+   }
+   public void  print(String text, int number) {
+	   System.out.println(text);
+	   System.out.println(number);
+   }
+}
 
-public void print() {
-   System.out.println("Better student");
-   System.out.println("First name:" + firstName);
-   System.out.println("Last name:" + lastName);
-   System.out.println("Rating:" + rating);
-}
-}
+
+
+
+
